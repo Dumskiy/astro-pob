05c7308227b7194113e3e704b1d726e82787404c
Andrey Dumskiy <a.dumskiy@modulbank.ru>
2025-07-17 23:57:56 +0300

    added sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   // scopedStyleStrategy: "class",
   devToolbar: {
     enabled: false
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/breakpoints" as *;`
+        }
+      }
+    }
   }
 });
